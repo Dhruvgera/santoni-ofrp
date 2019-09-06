cccb4d12dc4dbe6f6bf62dfb0e2cb337b3e9a6a6
Akmal <magchuz@pm.me>
2019-09-06 22:30:44 +0000

    Set BOARD_BUILD_SYSTEM_ROOT_IMAGE True

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/recovery/kernel
 
